e694b5e76f63d3d0620e43ad725c0da00eed2669
Laiq Jafri <laiq.jafri@gmail.com>
2015-06-25 21:35:02 +0500

    Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,11 @@
 class Url < ActiveRecord::Base
+  KEY_LENGTH = 5 #the length will actually be around 4/3 of this number
+  before_save :set_key
+  validates_presence_of :url
+  validates_format_of   :url, :with => URI::regexp(%w(http https))
+
+  def set_key
+    self.key = SecureRandom.urlsafe_base64(KEY_LENGTH)
+    self.set_key if Url.exists?(key: self.key)
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has none to begin with" do
+    expect(Url.count).to eq 0
+  end
+
+  it "will test an empty url" do
+    url = Url.create
+    expect(url.valid?).to be false
+    expect(Url.count).to eq 0
+  end
+
+  it "will test an invalid url" do
+    url = Url.create :url => "test-url"
+    expect(url.valid?).to be false
+    expect(Url.count).to eq 0
+  end
+
+  it "will save a valid url with http" do
+    url = Url.create :url => "http://l@iq.com"
+    expect(Url.count).to eq 1
+    expect(Url.first.url).to eql "http://l@iq.com"
+  end
+
+  it "will save a valid url with https" do
+    url = Url.create :url => "https://l@iq.com"
+    expect(Url.count).to eq 1
+    expect(Url.first.url).to eql "https://l@iq.com"
+  end
+
+  it "will test that set_key is called on save" do
+    url = Url.new :url => "http://l@iq.com"
+    expect(url).to receive(:set_key).once
+    url.save
+  end
+
+  it "will test url has a key present?" do
+    url = Url.create :url => "http://l@iq.com"
+    expect(Url.count).to eq 1
+    expect(Url.first.key.present?).to be true
+  end
 end
